7c26d8a1fd098c39e1cfc444406761073fc5e17d
MichelOliveiraNas <michel.oliveira999@hotmail.com>
2020-06-15 20:27:52 -0300

    Aprendendo sobre Type Inference

---
+++ Aula 03/type-inference.ts
@@ -0,0 +1,7 @@
+// Type Inference
+let message2 = "mensagem definida" // definir o tipo da variável implicitamente, apenas passando o seu valor.
+message2 = "string nova"
+
+window.addEventListener("click", (e) => {
+    console.log(e.target)
+})
